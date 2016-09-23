ec494c6d1357b0a6bc29cc71ea658d48f79c513d
Oleksandr Vasylenko <Oleksandr.Vasylenko@infopulse.local>
2016-09-23 11:39:07 +0300

    added redis for caching

--- routes/pet.js
+++ routes/pet.js
@@ -3,6 +3,10 @@ var r = require('request').defaults({
 });
 
 var async = require('async');
+var redis = require('redis');
+
+var client = redis.createClient(6379, '127.0.0.1');
+
 
 module.exports = function (app) {
 
@@ -24,19 +28,31 @@ module.exports = function (app) {
         });
       },
       dog: function (callback) {
-        r({url:'http://localhost:3001/dog'}, function (error, response, body) {
-          if(error){
-            callback({service:'dog', error: error});
-            return;
-          }
-          if(!error && response.statusCode === 200) {
-            callback(null, body);
+        client.get('http://localhost:3001/dog', function(error, dog){
+          if(error) { throw error; };
+          if(dog){
+            callback(null, JSON.parse(dog));
           } else {
-            callback(response.statusCode);
+            r({url:'http://localhost:3001/dog'}, function (error, response, body) {
+              if(error){
+                callback({service:'dog', error: error});
+                return;
+              }
+              if(!error && response.statusCode === 200) {
+                callback(null, data.body);
+                //Set 10 seconds for caching
+                client.setex('http://localhost:3001/dog', 10, JSON.stringify(body.data),function(error){
+                  if(error){ throw error;};
+                });
+              } else {
+                callback(response.statusCode);
+              }
+            });
           }
         });
       }
-    },function(error, results) {
+    },
+    function(error, results) {
       res.json({
         error: error,
         results: results
